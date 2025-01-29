839c464f9206733f99a2fc919eb1b7d9f9676182
Anshumaankhare2403 <anshumaan24khare1@gmail.com>
2025-01-29 18:03:15 +0530

    first commit

---
+++ JS1.js
@@ -0,0 +1,39 @@
+// variable 
+var a =10;
+console.log(a);
+a=100
+console.log(a)
+
+let b =10;
+console.log(b);
+b=100
+console.log(b)
+
+const c =10;
+console.log(c);
+let cc ;
+console.log(cc);
+
+xx =10;
+console.log(xx);
+// c =100;const variable neaver change the value of it   
+// console.log(c)
+
+
+// data Type 
+
+let n =10;
+let bb = "sdsd";
+let nn = null;
+let bi = BigInt("343");
+let ss = Symbol("aeexx");
+let sss;
+console.log(n,bb,nn,bi,ss,sss);
+
+const obj1 = {
+    "name":"anuj",
+    "age":24,
+    "good":true
+};
+
+console.log(obj1["age"],obj1["name"]);

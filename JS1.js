b1a98644368762e1ea85babc37f48927c3629106
Anshumaankhare2403 <anshumaan24khare1@gmail.com>
2025-01-31 20:59:02 +0530

    js

--- JS1.js
+++ JS1.js
@@ -1,39 +1,64 @@
-// variable 
-var a =10;
-console.log(a);
-a=100
-console.log(a)
+// // variable is a container to store the values 
+// // Let,Const,Var are the way to diclear 
+// var a =10;
+// console.log(a);
+// a=100
+// console.log(a)
 
-let b =10;
+// let b =10;
+// console.log(b);
+// b=100
+// console.log(b)
+
+// const c =10;
+// console.log(c);
+// let cc ;
+// console.log(cc);
+
+// xx =10;
+// console.log(xx);
+// // c =100;const variable neaver change the value of it   
+// // console.log(c)
+
+
+// // data Type 
+
+// let n =10;
+// let bb = "sdsd";
+// let nn = null;
+// let bi = BigInt("343");
+// let ss = Symbol("aeexx");
+// let sss;
+// console.log(n,bb,nn,bi,ss,sss);
+
+// const obj1 = {
+//     "name":"anuj",
+//     "age":24,
+//     "good":true
+// };
+
+// console.log(obj1["age"],obj1["name"]);
+
+// type conversion in js 
+
+let a =10;
+console.log(a);
+console.log(typeof(a));
+let c = String(a);
+console.log(typeof(c));
+a = "2.33";
+console.log(a);
+console.log(typeof(a));
+
+let b =Number(a);
 console.log(b);
-b=100
+b =parseFloat(a);
+console.log(b);
+b =parseInt(a);
+console.log(b);
+a=0;
+b = Boolean(a);
 console.log(b)
 
-const c =10;
-console.log(c);
-let cc ;
-console.log(cc);
-
-xx =10;
-console.log(xx);
-// c =100;const variable neaver change the value of it   
-// console.log(c)
 
 
-// data Type 
-
-let n =10;
-let bb = "sdsd";
-let nn = null;
-let bi = BigInt("343");
-let ss = Symbol("aeexx");
-let sss;
-console.log(n,bb,nn,bi,ss,sss);
-
-const obj1 = {
-    "name":"anuj",
-    "age":24,
-    "good":true
-};
-
-console.log(obj1["age"],obj1["name"]);
---
+++ JS2.js
@@ -0,0 +1,11 @@
+// function sum (a,b){
+//     let c = a+b;
+//     console.log(`sum = ${c}`);
+// }
+
+// sum(10,20);
+
+const sum = (a, b) => {
+    return console.log(`sum = ${a + b}`);
+}
+console.log(sum);
